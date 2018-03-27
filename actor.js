be1012760625b2732d739b9fd38198e58f515427
gingerale1221 <hvnguyen.jr@gmail.com>
2018-03-27 20:56:23 +0800

    Update actor.js

--- actor.js
+++ actor.js
@@ -17,7 +17,7 @@ AFRAME.registerComponent('line-draw', {
             //sphere.setAttribute('position', e.detail.intersection.point);
             cylinder.setAttribute('position', e.detail.intersection.point);
             curve_center.setAttribute('position', e.detail.intersection.point);
-            camera.setAttribute('position', {x: 0, y: 0, z: 0});
+            //camera.setAttribute('position', {x: 0, y: 0, z: 0});
             cylinder.setAttribute('height', 12);
             sphere.setAttribute('position', {x: 0, y: 6, z: 0})
             //camera.setAttribute('rotation', {x: 1.948, y: 88.865, z: -1.776});
