c444c166ce1ec7934c3d99e032660e03ca45db6c
gingerale1221 <hvnguyen.jr@gmail.com>
2018-04-02 17:59:17 +0800

    Add files via upload

---
+++ mask.js
@@ -0,0 +1,42 @@
+AFRAME.registerComponent('line-draw', {
+    // this.mouse = new THREE.Vector2();
+    // this.scene = this.el.sceneEl;
+    // this.camera = this.scene.camera;
+    // this.obj = this.el.object3D;
+    init: function() {
+               this.el.addEventListener('click', function (e) {
+               console.log(e.detail.intersection.point);
+    //         // let rc = new THREE.Raycaster();
+    //         // rc.setFromCamera(this.mouse, this.camera);
+    //         // let dist = this.obj.position.distanceTo(this.camera.position);
+    //         // let point = rc.ray.at(dist);
+    //         var camera = document.querySelector('#camera')
+    //         var sphere = document.querySelector('a-sphere');
+    //         var cylinder = document.querySelector('a-cylinder');
+    //         var curve_center = document.querySelector('#curve-point-center');
+    //         //sphere.setAttribute('position', e.detail.intersection.point);
+    //         cylinder.setAttribute('position', e.detail.intersection.point);
+    //         curve_center.setAttribute('position', e.detail.intersection.point);
+    //         //camera.setAttribute('position', {x: 0, y: 0, z: 0});
+    //         cylinder.setAttribute('height', 16);
+    //         sphere.setAttribute('position', {x: 0, y: 8, z: 0})
+    //         //camera.setAttribute('rotation', {x: 1.948, y: 88.865, z: -1.776});
+    //         console.log(camera.components.position);
+    //         console.log(cylinder.getDOMAttribute('geometry').height);
+    //         console.log(sphere.position);
+    //         console.log(sphere.position);
+           });
+          var triggerShake = document.createEvent("HTMLEvents");
+            triggerShake.initEvent("shake", true, true);
+
+          function shakeMe(){
+            document.dispatchEvent(triggerShake);
+          }
+}
+    // var triggerShake = document.createEvent("HTMLEvents");
+    // triggerShake.initEvent("shake", true, true);
+
+    // function shakeMe(){
+    //   document.dispatchEvent(triggerShake);
+    // }
+});
